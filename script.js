1c28b7b84abc2e5a2a090061572104b87677ad2d
Vishal Yadav <ydvishal89@gmail.com>
2026-02-21 09:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const byteSize = (str) => {
-  // write your code here
-	return new Blob([str]).size;
+  let bytes = 0;
+
+  for (let char of str) {
+    const code = char.codePointAt(0);
+
+    if (code <= 0x7F) {
+      bytes += 1;          // 1 byte (ASCII)
+    } else if (code <= 0x7FF) {
+      bytes += 2;          // 2 bytes
+    } else if (code <= 0xFFFF) {
+      bytes += 3;          // 3 bytes
+    } else {
+      bytes += 4;          // 4 bytes (emoji etc.)
+    }
+  }
+
+  return bytes;
 };
 
-Do not change the code below
+// Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
